07b2242600fef4615460c484e71a574ea88ade0d
ÖmerSargin <omersargin4116@hotmail.com>
2024-02-09 17:53:09 +0300

    Project added

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager:ISupplierService
+    {
+        private IApplicantService _applicantService; //constructordan ulaşmak için
+
+        //ihtiyac duyulan sınıfı newlemek yerine onun interfacı yazılır
+        //mikro servis implementesi.
+        public PttManager(IApplicantService applicantService) {
+            //constructor new yapılınca çalışır.
+            _applicantService = applicantService; //fieldların alt çizgiyle baslama sebebi.
+        }
+
+
+
+        public void GiveMask(Person person)
+        {
+            
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ.");
+            }
+        }
+    }
+}
